75c404a7d49ab3aec3dae7977be52d95afb1517b
山中佑介 <yamayu.7349@gmail.com>
2024-06-06 22:35:41 +0900

    finish chapter4

--- baby.hs
+++ baby.hs
@@ -1,5 +1,6 @@
 import Distribution.Simple.Utils (xargs)
-import Text.XHtml (height)
+import Text.XHtml (height, abbr)
+import Distribution.Compat.Lens (_1)
 {-# OPTIONS -Wall -Werror #-}
 doubleMe x = x + x
 doubleUs x y= x * 2 + y * 2
@@ -36,4 +37,43 @@ cylinder r h =
     let sideArea =2*pi*r*h
         topArea = pi * r^2
     in sideArea +2 *topArea
+
+maxium' :: (Ord a) => [a] -> a
+maxium' [] = error "maximum of empty list!"
+maxium' [x] = x
+maxium' (x:xs) = max x (maxium' xs)
+
+replicate' :: Int -> a-> [a]
+replicate' n x
+ | n <= 0    = []
+ | otherwise = x : replicate' (n-1) x
+
+take' :: Int -> [a] -> [a]
+take' n _
+    | n <= 0 = []
+take' _ [] = []
+take' n (x:xs) = x: take' (n-1) xs
+
+reverse' :: [a] -> [a]
+reverse' [] = []
+reverse' [x] = [x]
+reverse' (x:xs) = reverse' xs ++ [x]
+
+zip' :: [a] -> [b] -> [(a,b)]
+zip' [] _ = []
+zip' _ [] = []
+zip' (x:xs) (y:ys) = (x,y) : zip' xs ys
+
+elem' :: (Eq a) => a -> [a] -> Bool
+elem' _ [] = False
+elem' a (x:xs)
+    | a == x = True
+    | otherwise = elem' a xs
+
+quickSort :: (Ord a) => [a] ->[a]
+quickSort [] = []
+quickSort (x:xs) =
+    let smallerOrEqual = [a | a <-xs, a<=x]
+        larger = [a | a<-xs, a>x]
+    in quickSort smallerOrEqual ++ [x] ++ quickSort larger
     
